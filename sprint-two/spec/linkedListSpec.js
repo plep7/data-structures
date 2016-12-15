b71500e6cd602e0ef90710fdcd31bee462807fbb
psheehan89 <psheehan89@gmail.com>
2016-12-14 21:07:44 -0800

    Completed linkedList, need to clean up console.logs in spec

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -26,6 +26,7 @@ describe('linkedList', function() {
   it('should remove the head from the list when removeHead is called', function() {
     linkedList.addToTail(4);
     linkedList.addToTail(5);
+    console.log(linkedList.head);
     expect(linkedList.head.value).to.equal(4);
     linkedList.removeHead();
     expect(linkedList.head.value).to.equal(5);
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+
+    if (list.head === null) {
+      var node = Node(value);
+      list.tail = node;
+      list.head = list.tail;    //{value: 1, next: null} list.head = {value: 1, next: null} list.tail = {value: 1, next: null}
+    }
+    else {
+      node = Node(value);
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var temp = list.head;
+    list.head = list.head.next;
+    return temp.value;
+
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while(currentNode) {
+      if (target === currentNode.value) {
+        return true;
+      }
+      else {
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
   };
 
   return list;
